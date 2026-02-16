ecefaeda542c4cdf4e52249b6680b81aa82c9fc0
LuketheName56 <LuketheName56@gmail.com>
2026-02-15 19:26:30 -0800

    Changed Local Parameter

--- Assets/Scripts/CharacterInteractions/MusicEnemy.cs
+++ Assets/Scripts/CharacterInteractions/MusicEnemy.cs
@@ -4,20 +4,19 @@ using FMOD.Studio;
 
 public class MusicEnemy : MonoBehaviour
 {
-    FMOD.Studio.EventInstance BG_Music;
+    private StudioEventEmitter studioEventEmitter;
     public string parameterName = "Combat_Intensity"; // Name defined in FMOD Studio
     public float enemyMode = 0.0f;
     
 
-    // Example of updating the parameter dynamically (e.g., every frame)
-    void Start()
+    void Awake()
     {
-        BG_Music = FMODUnity.RuntimeManager.CreateInstance("event:/Bg_Level_2");
+        studioEventEmitter = ServiceLocator.Get<StudioEventEmitter>();
     }
     
     void Update()
     {
-        BG_Music.setParameterByName(parameterName, enemyMode);
+        studioEventEmitter.SetParameter(parameterName, enemyMode);
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
