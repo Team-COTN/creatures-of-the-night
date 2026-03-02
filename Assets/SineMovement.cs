993763da227ad57d4d86fc0d6562ea5086722a08
LuketheName56 <LuketheName56@gmail.com>
2026-03-02 08:46:07 -0800

    Easier Level Beginning

--- Assets/SineMovement.cs
+++ Assets/SineMovement.cs
@@ -7,6 +7,8 @@ public class SineMovement : MonoBehaviour
     
     // The speed of the movement (higher value means faster oscillation)
     public float speed = 2f; 
+    public float offset = 3.14159f/2f; 
+
 
     private Vector3 startPosition;
 
@@ -22,7 +24,7 @@ public class SineMovement : MonoBehaviour
         // Time.time gives a continuously increasing value
         // Mathf.Sin(Time.time * speed) oscillates between -1 and 1
         // We multiply by movementDistance / 2 to get the desired range (e.g., -2.5 to 2.5)
-        float newY = startPosition.y + Mathf.Sin(Time.time * speed) * (movementDistance / 2f);
+        float newY = startPosition.y + Mathf.Sin(Time.time * speed + offset) * (movementDistance / 2f);
 
         // Update the object's position
         transform.position = new Vector3(startPosition.x, newY, startPosition.z);
